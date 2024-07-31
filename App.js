1c2a9a6dfa314e01c469056d6692efc57bc19410
赵晨曦 <zhaochenxi@zhaochenxideair.mynetworksettings.com>
2024-07-30 20:52:02 -0400

    urlKey

--- App.js
+++ App.js
@@ -5,45 +5,51 @@ import session from "express-session";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
-import AssignmentRoutes from "./Kanbas/Assignments/routes.js"
+import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import UserRoutes from "./User/routes.js";
 import cors from "cors";
 import mongoose from "mongoose";
-import "dotenv/config";
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas-suf";
-mongoose.connect(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const app = express();
 app.use(
-    cors({
-      credentials: true,
-      origin: process.env.NETLIFY_URL || "http://localhost:3000",
-    })
-   );
-   const sessionOptions = {
-    secret: process.env.SESSION_SECRET || "kanbas",
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({ CONNECTION_STRING }),
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:3000",
+  })
+);
+
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "kanbas",
+  resave: false,
+  saveUninitialized: false,
+  store: MongoStore.create({ mongoUrl: CONNECTION_STRING }),
+};
+
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
   };
-  if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-      sameSite: "none",
-      secure: true,
-      domain: process.env.NODE_SERVER_DOMAIN,
-    };
-  }
-  app.use(session(sessionOptions));
-  
-     
-app.use(express.json()); // do all your work after this line
+}
+
+app.use(session(sessionOptions));
+app.use(express.json()); // Make sure all routes are defined after this line
+
 UserRoutes(app);
-app.get('/', (req, res) => {
-    res.send('Welcome to Full Stack Development!');
+app.get("/", (req, res) => {
+  res.send("Welcome to Full Stack Development!");
 });
-CourseRoutes(app);     
-ModuleRoutes(app); 
-AssignmentRoutes(app);         
-Lab5(app);                        
-app.listen(process.env.PORT || 4000);
+CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
+Lab5(app);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
